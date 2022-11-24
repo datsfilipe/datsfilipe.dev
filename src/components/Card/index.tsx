9ffcd7349e1f527309ed1c529ea93fa018483de4
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-24 19:31:43 -0300

    refactor(cards): update styles to have better constrast of text in background

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,7 +6,7 @@ interface CardProps {
 
 export function Card ({ children }: CardProps) {
   return (
-    <div className='flex p-4 w-full rounded-lg bg-noise-bg bg-stone-300 text-neutral-600 dark:text-neutral-400 dark:bg-zinc-900'>
+    <div className='flex p-4 w-full rounded-lg bg-noise-bg bg-stone-50 text-neutral-700 dark:text-neutral-300 dark:bg-zinc-900'>
       {children}
     </div>
   )
