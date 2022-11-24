663718350c3174e087445370766a4ca17a5ad048
datsfilipe <datsfilipe.dev@gmail.com>
2022-11-24 20:04:09 -0300

    refactor(componentizing): migrate cards logic to about card component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,10 @@
 import { Title } from '../../components/Title'
 import { Paragraph } from '../../components/Paragraph'
 import { Section } from '../../components/Section'
-import { Card } from '../../components/Card'
 import { CustomLink } from '../../components/CustomLink'
-import { MdCircle } from 'react-icons/md'
 import Image from 'next/image'
 import { CardsList } from '../../components/CardsList'
+import { AboutCard } from 'src/components/AboutCard'
 
 const comeFromLeftAnimation = {
   hidden: { opacity: 0, x: -100 },
@@ -60,63 +59,30 @@ export default function About () {
           Experience
         </Title>
         <CardsList listId={0}>
-          <Card>
-            <div className='mr-2 w-32 h-full max-md:w-16'>
-              2022 - Now
-            </div>
-            <div className='w-full'>
-              <em className='flex flex-wrap'>
-                Front-end Developer -&nbsp;<CustomLink href='https://casadosdados.com.br/solucao/cnpj/octaldev-solucoes-e-tecnologia-ltda-40237988000121' target='_blank'>Octal Dev</CustomLink>
-              </em>
-              <Paragraph className='mt-2 mb-2'>
-                Creator of tools such as the component library and the boilerplates being used alongside the projects. Currently building an website for users to find and allocate buffets for events like an e-commerce platform. Collaborated closely with the the project managers to track the objectives and stabilish deliveries.
-              </Paragraph>
-              <div className='flex flex-wrap items-center'>
-                <span className='flex items-center'><MdCircle />&nbsp;React</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;CSSModules</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;WebSockets</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Context API</span>
-              </div>
-            </div>
-          </Card>
-          <Card>
-            <div className='mr-2 w-32 h-full max-md:w-16'>
-              2018 - 2018
-            </div>
-            <div className='w-full'>
-              <em className='flex flex-wrap'>
-                Monitor in Computer Technician Course Extension Project -&nbsp;<CustomLink href='https://portal.ifma.edu.br/inicio' target='_blank'>IFMA</CustomLink>
-              </em>
-              <Paragraph className='mt-2 mb-2'>
-                I was a monitor in the Computer Technician Course Extension Project, whose objective was to bring this knowledge to students from other technical courses. 60 hours of workload.
-              </Paragraph>
-              <div className='flex flex-wrap items-center'>
-                <span className='flex items-center'><MdCircle />&nbsp;Windows</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Office</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Internet</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Hardware</span>&nbsp;
-              </div>
-            </div>
-          </Card>
-          <Card>
-            <div className='mr-2 w-32 h-full max-md:w-16'>
-              2018 - 2021
-            </div>
-            <div className='w-full'>
-              <em className='flex flex-wrap'>
-                Computer Technician -&nbsp;<CustomLink href='https://portal.ifma.edu.br/inicio' target='_blank'>IFMA</CustomLink>
-              </em>
-              <Paragraph className='mt-2 mb-2'>
-                I was an excellent programming student, which gave me the opportunity to participate in events such as FLISOL (Latin American Free Software Installation Festival), where I fell in love with Linux and open source. I finished high school as a Computer Technician at the Federal Institute of Maranhão.
-              </Paragraph>
-              <div className='flex flex-wrap items-center'>
-                <span className='flex items-center'><MdCircle />&nbsp;Programming logic</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Web Development</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Networks</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Hardware</span>&nbsp;
-              </div>
-            </div>
-          </Card>
+          <AboutCard
+            year='2022 - Now'
+            title='Front-end Developer - Octal Dev'
+            reference='https://casadosdados.com.br/solucao/cnpj/octaldev-solucoes-e-tecnologia-ltda-40237988000121'
+            tags={['React', 'CSSModules', 'WebSockets', 'Context API']}
+          >
+            Creator of tools such as the component library and the boilerplates being used alongside the projects. Currently building an website for users to find and allocate buffets for events like an e-commerce platform. Collaborated closely with the the project managers to track the objectives and stabilish deliveries.
+          </AboutCard>
+          <AboutCard
+            year='2018 - 2018'
+            title='Monitor in Computer Technician Course Extension Project - IFMA'
+            reference='https://portal.ifma.edu.br/inicio'
+            tags={['Hardware', 'Windows', 'Office', 'Internet']}
+          >
+            I was a monitor in the Computer Technician Course Extension Project, whose objective was to bring this knowledge to students from other technical courses. 60 hours of workload.
+          </AboutCard>
+          <AboutCard
+            year='2018 - 2021'
+            title='Computer Technician - IFMA'
+            reference='https://portal.ifma.edu.br/inicio'
+            tags={['Programming logic', 'Web Development', 'Networks', 'Hardware']}
+          >
+            I was an excellent programming student, which gave me the opportunity to participate in events such as FLISOL (Latin American Free Software Installation Festival), where I fell in love with Linux and open source. I finished high school as a Computer Technician at the Federal Institute of Maranhão.
+          </AboutCard>
         </CardsList>
       </Section>
       <Section
@@ -127,82 +93,38 @@ export default function About () {
           Side Projects
         </Title>
         <CardsList listId={1}>
-          <Card>
-            <div className='mr-2 w-32 h-full max-md:w-16'>
-              2022
-            </div>
-            <div className='w-full'>
-              <em className='flex flex-wrap'>
-                React Bad Apple -&nbsp;<CustomLink href='http://react-bad-apple.vercel.app/' target='_blank'>Repository</CustomLink>
-              </em>
-              <Paragraph className='mt-2 mb-2'>
-                It&apos;s an npm package that allows user to transform frame images into an ascii playable component and use alongside React projects.
-              </Paragraph>
-              <div className='flex flex-wrap items-center'>
-                <span className='flex items-center'><MdCircle />&nbsp;React</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;ESBuild</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Typescript</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Vite</span>&nbsp;
-              </div>
-            </div>
-          </Card>
-          <Card>
-            <div className='mr-2 w-32 h-full max-md:w-16'>
-              2021
-            </div>
-            <div className='w-full'>
-              <em className='flex flex-wrap'>
-                Ignews -&nbsp;<CustomLink href='https://ignews-datsfilipe.vercel.app/' target='_blank'>Repository</CustomLink>
-              </em>
-              <Paragraph className='mt-2 mb-2'>
-                I&apos;s an news in post format app. It was made with a lot of cool technologies and librarys, such as Stripe, Next Auth, PrismicCMS, FaunaDB, etc. The focus was learning about CSR (Client Side Rendering), SSR (Server Side Rendering) and SSG (Static Site Generation).
-              </Paragraph>
-              <div className='flex flex-wrap items-center'>
-                <span className='flex items-center'><MdCircle />&nbsp;React</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Next.js</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Typescript</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Jest</span>&nbsp;
-              </div>
-            </div>
-          </Card>
-          <Card>
-            <div className='mr-2 w-32 h-full max-md:w-16'>
-              2022
-            </div>
-            <div className='w-full'>
-              <em className='flex flex-wrap'>
-                React Jokenpo -&nbsp;<CustomLink href='https://react-jokenpo-alpha.vercel.app/' target='_blank'>Repository</CustomLink>
-              </em>
-              <Paragraph className='mt-2 mb-2'>
-                I&apos;s an rock, scissor, paper game kind of different. In this game you pick up your winner and see if it stands up until the end. I was inspired by an random gif that I saw at internet, hope you like.
-              </Paragraph>
-              <div className='flex flex-wrap items-center'>
-                <span className='flex items-center'><MdCircle />&nbsp;React</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Context API</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Typescript</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Vite</span>&nbsp;
-              </div>
-            </div>
-          </Card>
-          <Card>
-            <div className='mr-2 w-32 h-full max-md:w-16'>
-              2022
-            </div>
-            <div className='w-full'>
-              <em className='flex flex-wrap'>
-                datsfilipe.dev -&nbsp;<CustomLink href='https://github.com/datsfilipe/datsfilipe.dev' target='_blank'>Repository</CustomLink>
-              </em>
-              <Paragraph className='mt-2 mb-2'>
-                It&apos;s this very place we are right now. Personal website to show some attributes I have. I&apos;m also building it in Next.js version 13 with all the experimental features.
-              </Paragraph>
-              <div className='flex flex-wrap items-center'>
-                <span className='flex items-center'><MdCircle />&nbsp;React</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Next.js</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Typescript</span>&nbsp;
-                <span className='flex items-center'><MdCircle />&nbsp;Framer Motion</span>&nbsp;
-              </div>
-            </div>
-          </Card>
+          <AboutCard
+            year='2022'
+            title='React Bad Apple - Deploy'
+            reference='http://react-bad-apple.vercel.app/'
+            tags={['React', 'ESBuild', 'Typescript', 'Vite']}
+          >
+            It&apos;s an npm package that allows user to transform frame images into an ascii playable component and use alongside React projects.
+          </AboutCard>
+          <AboutCard
+            year='2021'
+            title='Ignews - Deploy'
+            reference='https://ignews-datsfilipe.vercel.app/'
+            tags={['React', 'Next.js', 'Typescript', 'Jest']}
+          >
+            I&apos;s an news in post format app. It was made with a lot of cool technologies and librarys, such as Stripe, Next Auth, PrismicCMS, FaunaDB, etc. The focus was learning about CSR (Client Side Rendering), SSR (Server Side Rendering) and SSG (Static Site Generation).
+          </AboutCard>
+          <AboutCard
+            year='2022'
+            title='React Jokenpo - Deploy'
+            reference='https://react-jokenpo-alpha.vercel.app/'
+            tags={['React', 'Context API', 'Typescript', 'Vite']}
+          >
+            I&apos;s an rock, scissor, paper game kind of different. In this game you pick up your winner and see if it stands up until the end. I was inspired by an random gif that I saw at internet, hope you like.
+          </AboutCard>
+          <AboutCard  
+            year='2022'
+            title='datsfilipe.dev - Deploy'
+            reference='https://datsfilipe.dev/'
+            tags={['React', 'Next.js', 'Typescript', 'Framer Motion']}
+          >
+            It&apos;s this very place we are right now. Personal website to show some attributes I have. I&apos;m also building it in Next.js version 13 with all the experimental features.
+          </AboutCard>
         </CardsList>
         <Paragraph className='text-neutral-600 dark:text-neutral-400'>
           In order to see more of my personal projects/repositorys, please visit my github profile and take a look, here&apos;s the link:&nbsp;<CustomLink href='https://github.com/datsfilipe' target='_blank'>datsfilipe</CustomLink>
---
+++ src/components/AboutCard/index.tsx
@@ -0,0 +1,40 @@
+import type { ReactNode } from 'react'
+import { MdCircle } from 'react-icons/md'
+import { Card } from '../Card'
+import { CustomLink } from '../CustomLink'
+import { Paragraph } from '../Paragraph'
+
+interface AboutCardProps {
+  title: string;
+  reference: string;
+  tags: string[];
+  year: string;
+  children: ReactNode;
+}
+
+export function AboutCard ({ title, reference, tags, year, children }: AboutCardProps) {
+  const [ titlePiece, referencePiece ] = title.split('-')
+
+  return (
+    <Card>
+      <div className='mr-2 w-32 h-full max-md:w-16'>
+        {year}
+      </div>
+      <div className='w-full'>
+        <em className='flex flex-wrap'>
+          {titlePiece}&nbsp;<CustomLink href={reference} target='_blank'>{referencePiece}</CustomLink>
+        </em>
+        <Paragraph className='mt-2 mb-2'>
+          {children}
+        </Paragraph>
+        <div className='flex flex-wrap items-center'>
+          {tags.map((tag, index) => (
+            <span key={index} className='flex items-center'>
+              <MdCircle />&nbsp;{tag}&nbsp;
+            </span>
+          ))}
+        </div>
+      </div>
+    </Card>
+  )
+}
