22a375f8e9ce945bdf0708d95ec905ba66799f5c
datsfilipe <datsfilipe.pro@proton.me>
2023-04-14 21:35:15 -0300

    styles (button): add styles to button using tailwind-variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,10 +1,32 @@
+import { tv } from 'tailwind-variants'
+
 type ButtonProps = {
   children: React.ReactNode
-} & React.ButtonHTMLAttributes<HTMLButtonElement>
+  use?: 'primary' | 'danger' | 'link' | 'buttonLink'
+  rounded?: 'sm' | 'md' | 'lg' | 'full'
+} & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'className'>
 
-export const Button = ({ children, ...props }: ButtonProps) => {
+export const Button = ({ children, use = 'buttonLink', rounded = 'md', ...props }: ButtonProps) => {
+  const button = tv({
+    base: 'font-bold py-2 px-4',
+    variants: {
+      type: {
+        primary: 'dark:bg-zinc-700 dark:text-white bg-zinc-300 text-black',
+        danger: 'bg-red-500 text-white',
+        link: 'bg-transparent text-zinc-700 dark:text-white',
+        buttonLink: 'dark:bg-zinc-700 dark:text-white bg-zinc-300 text-black',
+      },
+      rounded: {
+        sm: 'rounded-sm',
+        md: 'rounded-md',
+        lg: 'rounded-lg',
+        full: 'rounded-full',
+      },
+    },
+  })
+
   return (
-    <button {...props}>
+    <button className={button({ type: use, rounded })} {...props}>
       {children}
     </button>
   )
