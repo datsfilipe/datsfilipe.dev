1b937c62279cafff20b9146dddc96a8087bb7f0f
datsfilipe <datsfilipe.pro@proton.me>
2023-04-28 10:30:22 -0300

    refactor(navigation): add as dropdown for lower resolutions

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,43 +4,97 @@ import Link from 'next/link'
 import { Icons } from '@/components/icons'
 import { ToggleTheme } from '@/components/toggleTheme'
 import { usePathname } from 'next/navigation'
+import { Button } from './ui/button'
+import { useEffect, useState } from 'react'
 
 const navigationVariants = tv({
   slots: {
-    nav: 'flex items-center justify-between flex-wrap p-4',
-    ul: 'list-reset flex flex-col ml-4',
-    item: '[&>a]:flex [&>a]:items-center [&>a>svg]:mr-2 mb-4 font-serif font-medium text-lg text-gray-400 hover:text-gray-200 transition-colors duration-200',
+    normalNav: 'flex items-center ml-4 justify-between flex-wrap p-4 sticky',
+    ul: 'list-reset flex flex-col',
+    item: '[&>a]:flex [&>a]:items-center [&>a>svg]:mr-2 mb-4 font-serif font-medium text-lg hover:text-gray-800 hover:dark:text-gray-200 transition-colors duration-200',
   },
 })
 
-export const Navigation = () => {
-  let pathname = usePathname() || '/'
+const List = ({ pathname, ...props }: { pathname: string } & React.HTMLAttributes<HTMLUListElement>) => (
+  <ul className={navigationVariants().ul()} {...props}>
+    <li className={navigationVariants().item() + (pathname === '/' ? ' text-gray-800 dark:text-gray-200' : ' text-gray-600 dark:text-gray-400')}>
+      <Link href="/">
+        <Icons.Home />
+        Home
+      </Link>
+    </li>
+    <li className={navigationVariants().item() + (pathname === '/blog' ? ' text-gray-800 dark:text-gray-200' : ' text-gray-600 dark:text-gray-400')}>
+      <Link href="/blog">
+        <Icons.WritingSign />
+        Blog
+      </Link>
+    </li>
+    <li>
+      <ToggleTheme />
+    </li>
+  </ul>
+)
+
+const Dropdown = (props: { children: React.ReactNode }) => {
+  const [open, setOpen] = useState(false)
 
   return (
-    <nav className={navigationVariants().nav()}>
-      <ul className={navigationVariants().ul()}>
-        <li className={navigationVariants().item() + (pathname === '/' ? ' text-gray-200' : '')}>
-          <Link href="/">
-            <Icons.Home />
-            Home
-          </Link>
-        </li>
-        <li className={navigationVariants().item() + (pathname === '/blog' ? ' text-gray-200' : '')}>
-          <Link href="/blog">
-            <Icons.WritingSign />
-            Blog
-          </Link>
-        </li>
-        <li className={navigationVariants().item() + (pathname === '/guestbook' ? ' text-gray-200' : '')}>
-          <Link href="/guestbook">
-            <Icons.Message />
-            Guestbook
-          </Link>
-        </li>
-        <li>
-          <ToggleTheme />
-        </li>
-      </ul>
+    <div className="absolute top-10 right-10">
+      <div className="relative">
+        <Button onClick={() => setOpen(!open)}>
+          <Icons.Menu />
+        </Button>
+        {open && (
+          <div className="absolute top-0 right-0 p-4 mt-12 bg-white rounded-lg shadow-lg dark:bg-zinc-800">
+            {props.children}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export const Navigation = () => {
+  const [windowWidth, setWindowWidth] = useState(768)
+  const [mounted, setMounted] = useState(false)
+
+  let pathname = usePathname() || '/'
+  if (pathname.includes('/blog/')) {
+    pathname = '/blog'
+  }
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  if (windowWidth < 768) {
+    return (
+      <Dropdown>
+        <List pathname={pathname} />
+      </Dropdown>
+    )
+  }
+
+  return (
+    <nav className={navigationVariants().normalNav()}>
+      <List pathname={pathname}/>
     </nav>
   )
 }
