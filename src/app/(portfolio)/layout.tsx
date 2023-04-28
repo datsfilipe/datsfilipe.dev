cd674c7b2ac5053c3b0b6aef1ab4c823ba93fbe7
datsfilipe <datsfilipe.pro@proton.me>
2023-04-28 10:31:31 -0300

    refactor(layouts): update styles and remove metadata

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,15 +1,14 @@
 import { Navigation } from '@/components/navigation'
 
-export const metadata = {
-  title: 'datsfilipe',
-  description: 'Filipe Lima personal website.',
-}
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex justify-center items-center py-2 mx-auto max-w-4xl min-h-screen">
-      {children}
-      <Navigation/>
+    <div className="flex relative justify-center items-center mx-auto max-w-4xl min-h-screen">
+      <div className="flex justify-center items-center my-10 mx-auto w-full md:my-2">
+        {children}
+        <div className="flex top-0 flex-col justify-center self-start -mt-2 md:sticky md:h-screen">
+          <Navigation />
+        </div>
+      </div>
     </div>
   )
 }
--- src/app/bio/layout.tsx
+++ src/app/bio/layout.tsx
@@ -1,8 +1,3 @@
-export const metadata = {
-  title: 'datsfilipe | bio',
-  description: 'Bio page of Filipe Lima',
-}
-
 export default function RootLayout({
   children,
 }: {
