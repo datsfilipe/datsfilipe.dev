d260037ffbfd2801ac61112d0c4f339000ab649a
datsfilipe <datsfilipe.pro@proton.me>
2023-04-28 10:41:02 -0300

    feat: add robots and sitemap

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://datsfilipe.dev/sitemap.xml',
+    host: 'https://datsfilipe.dev',
+  }
+}
