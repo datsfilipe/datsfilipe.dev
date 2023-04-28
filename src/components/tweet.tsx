22d5b6da24d3600e4eaae46e9e345dbce180d907
datsfilipe <datsfilipe.foss@proton.me>
2023-04-28 16:24:03 -0300

    feat(tweet): add priority to image and remove use client directive

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,4 +1,3 @@
-'use client'
 /* eslint-disable jsx-a11y/alt-text */
 import { Tweet as TweetComponent } from 'react-tweet'
 import Image from 'next/image'
@@ -6,7 +5,7 @@ import type { TweetComponents } from 'react-tweet'
 
 const components: TweetComponents = {
   AvatarImg: (props) => <Image {...props} className='!m-0' />,
-  MediaImg: (props) => <Image {...props} fill unoptimized />,
+  MediaImg: (props) => <Image {...props} fill priority unoptimized />,
 }
 
 const Tweet = ({ id }: { id: string }) => {
