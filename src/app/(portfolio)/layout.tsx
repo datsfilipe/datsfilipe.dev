d766e3faad57df2b5b3f01f71aa37149a70565d2
datsfilipe <datsfilipe.foss@proton.me>
2023-05-07 12:35:01 -0300

    refactor: update portfolio layout styles and content

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,4 +1,8 @@
 import { Navigation } from '@/components/navigation'
+import { appVariants } from '@/styles/appVariants'
+import { COPYRIGHT } from '@/components/copyright'
+import Image from 'next/image'
+import icon from '@/app/icon.svg'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   if (process.env.NODE_ENV !== 'development') return (
@@ -8,13 +12,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   )
 
   return (
-    <div className="flex relative justify-center items-center mx-auto max-w-4xl min-h-screen">
-      <div className="flex justify-center items-center my-10 mx-auto w-full md:my-2">
-        {children}
-        <div className="flex top-0 flex-col justify-center self-start -mt-2 md:sticky md:h-screen">
-          <Navigation />
-        </div>
+    <>
+      <div className='flex justify-between items-center mt-4 mx-4'>
+        <Image
+          src={icon as string}
+          alt='datsfilipe icon'
+          width={32}
+          height={32}
+          className='filter invert dark:invert-0'
+        />
+        <Navigation />
       </div>
-    </div>
+      <main className={appVariants.page()}>
+        {children}
+      </main>
+      <footer className='flex justify-center items-center'>
+        <p className='text-sm text-gray-500'>{COPYRIGHT}</p>
+      </footer>
+    </>
   )
 }
