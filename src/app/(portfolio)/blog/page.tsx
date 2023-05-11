f53beadd6dec9e0eef3bb2b3998363a8a05daae4
datsfilipe <datsfilipe.foss@proton.me>
2023-05-10 23:40:04 -0300

    styles: update blog list page

--- src/app/(portfolio)/blog/page.tsx
+++ src/app/(portfolio)/blog/page.tsx
@@ -1,50 +1,45 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
 import { allBlogs } from 'contentlayer/generated'
-import { tv } from 'tailwind-variants'
+import { text, link, container, heading } from '@/styles/appVariants'
 
 export const metadata: Metadata = {
   title: 'datsfilipe | blog',
   description: 'My collection of posts about software development (mostly) and other things.',
 }
 
-const blogVariants = tv({
-  slots: {
-    main: 'flex flex-col justify-center items-center mx-6 w-full h-full',
-    section: 'flex flex-col m-6 w-full h-full',
-    title: 'mb-5 font-serif text-3xl font-bold',
-    link: 'flex flex-col mb-4 space-y-1',
-    blog: 'flex flex-col w-full',
-  },
-})
-
 export default function Blog() {
   return (
-    <main className={blogVariants().main()}>
-      <section className={blogVariants().section()}>
-        <h1 className={blogVariants().title()}>Blog</h1>
-        {allBlogs.length ? allBlogs
-          .sort((a, b) => {
-            if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-              return -1
-            }
-            return 1
-          })
-          .map((post) => (
-            <Link
-              key={post.slug}
-              className={blogVariants().link()}
-              href={`/blog/${post.slug}`}
-            >
-              <div className={blogVariants().blog()}>
-                <p>{post.title}</p>
-              </div>
-            </Link>
-          )) : <div className='flex flex-col mt-6 text-gray-600 dark:text-gray-400'>
-            <p>There are no posts yet.</p>
-            <b className='mt-2'>Check back later! 🫠</b>
-          </div>}
-      </section>
-    </main>
+    <section className={container() + ' mt-24'}>
+      <h1 className={heading()}>Blog</h1>
+      {allBlogs.length ? allBlogs
+        .sort((a, b) => {
+          if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
+            return -1
+          }
+          return 1
+        })
+        .map((post) => (
+          <Link
+            key={post.slug}
+            href={`/blog/${post.slug}`}
+          >
+            <div className='mt-4'>
+              <p className={link()}><b>{post.title}</b></p>
+              <p className={text()}>{
+                new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
+                  timeZone: 'UTC'
+                }).format(new Date(post.publishedAt))
+              }</p>
+            </div>
+          </Link>
+        )) : <div className={text() + ' mt-4'}>
+          <p>There are no posts yet.</p>
+          <b className='mt-2'>Check back later! 🫠</b>
+        </div>}
+    </section>
   )
 }
