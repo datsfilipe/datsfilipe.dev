08ed66e253d1e53842628bd248f054cf758ec559
datsfilipe <datsfilipe.foss@proton.me>
2023-05-10 23:33:12 -0300

    feat: add src/styles folder to tailwind paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = withTV({
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/styles/**/*.{js,ts,jsx,tsx}',
     './content/**/*.mdx',
   ],
   theme: {
