b3b7c0ae29d801dbcaedb6cced23ea7742e62d4b
datsfilipe <datsfilipe.foss@proton.me>
2023-05-10 23:34:49 -0300

    refactor: remove some mdx components

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Image, { type ImageProps } from 'next/image'
 import { useMDXComponent } from 'next-contentlayer/hooks'
 import { Tweet } from '@/components/tweet'
-import { Icons } from '@/components/icons'
+import { post } from '@/styles/appVariants'
 
 const CustomLink = ({ href, ...props}: { href?: string; children?: React.ReactNode }) => {
   if (href?.startsWith('/')) {
@@ -25,57 +25,9 @@ function RoundedImage({ alt, ...props }: ImageProps) {
   return <Image alt={alt} className="rounded-md" {...props} />
 }
 
-function Callout(props: { emoji: React.ReactNode; children: React.ReactNode }) {
-  return (
-    <div className="flex p-4 my-8 rounded-md border bg-neutral-100 border-neutral-200 dark:bg-neutral-900 dark:border-neutral-800">
-      <div className="flex items-center mr-4 w-4">{props.emoji}</div>
-      <div className="w-full callout">{props.children}</div>
-    </div>
-  )
-}
-
-function ProsCard({ title, pros }: { title: string; pros: string[] }) {
-  return (
-    <div className="p-6 my-4 w-full rounded-md border border-emerald-200 dark:border-emerald-900 bg-neutral-50 dark:bg-neutral-900">
-      <span>{`You might use ${title} if...`}</span>
-      <div className="mt-4">
-        {pros.map((pro) => (
-          <div key={pro} className="flex items-baseline mb-2 font-medium">
-            <div className="mr-2 w-4 h-4">
-              <Icons.Checkbox className='text-emerald-500 mt-[2px]' />
-            </div>
-            <span>{pro}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
-
-function ConsCard({ title, cons }: { title: string; cons: string[] }) {
-  return (
-    <div className="p-6 my-6 w-full rounded-md border border-red-200 dark:border-red-900 bg-neutral-50 dark:bg-neutral-900">
-      <span>{`You might not use ${title} if...`}</span>
-      <div className="mt-4">
-        {cons.map((con) => (
-          <div key={con} className="flex items-baseline mb-2 font-medium">
-            <div className="mr-2 w-4 h-4">
-              <Icons.X className='text-red-500 mt-[2px]' />
-            </div>
-            <span>{con}</span>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
-
 const components = {
   Image: RoundedImage,
   a: CustomLink,
-  Callout,
-  ProsCard,
-  ConsCard,
 }
 
 interface MdxProps {
@@ -91,7 +43,7 @@ export function Mdx({ code, tweetIds }: MdxProps) {
   }
 
   return (
-    <article className="prose prose-quoteless prose-neutral dark:prose-invert">
+    <article className={post() + ' mb-14 text-base'}>
       <Component components={{ ...components, StaticTweet }} />
     </article>
   )
