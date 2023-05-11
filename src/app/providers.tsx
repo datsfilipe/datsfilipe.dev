0e1087a22f67275ebbdddde43115fbdc377f207f
datsfilipe <datsfilipe.foss@proton.me>
2023-05-10 23:36:34 -0300

    refactor: update next-themes provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,7 @@
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider attribute='class' defaultTheme='system' disableTransitionOnChange>
+  return <ThemeProvider attribute='class'>
     {children}
   </ThemeProvider>
 }
