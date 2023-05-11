4cd1e4c47621ad6d507169e4d307c0573f8e4f2a
datsfilipe <datsfilipe.foss@proton.me>
2023-05-10 23:39:05 -0300

    refactor: portfolio layout

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -1,8 +1,6 @@
 import { Navigation } from '@/components/navigation'
-import { appVariants } from '@/styles/appVariants'
-import { COPYRIGHT } from '@/components/copyright'
-import Image from 'next/image'
-import icon from '@/app/icon.svg'
+import { page } from '@/styles/appVariants'
+import { Copyright } from '@/components/copyright'
 import { generateRssFeed } from '@/utils/rss'
 
 generateRssFeed()
@@ -16,21 +14,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <>
-      <div className='flex justify-between items-center mt-4 mx-4'>
-        <Image
-          src={icon as string}
-          alt='datsfilipe icon'
-          width={32}
-          height={32}
-          className='filter invert dark:invert-0'
-        />
+      <main className={page()}>
         <Navigation />
-      </div>
-      <main className={appVariants.page()}>
         {children}
       </main>
-      <footer className='flex justify-center items-center'>
-        <p className='text-sm text-gray-500'>{COPYRIGHT}</p>
+      <footer className='flex justify-center my-8 items-center'>
+        <p className='text-gray-500'>
+          <Copyright />
+        </p>
       </footer>
     </>
   )
