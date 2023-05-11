b82b141478fa6033a724fe02ea0460bf5fb867eb
datsfilipe <datsfilipe.foss@proton.me>
2023-05-10 23:51:44 -0300

    remove check for production in main branch

--- src/app/(portfolio)/layout.tsx
+++ src/app/(portfolio)/layout.tsx
@@ -6,12 +6,6 @@ import { generateRssFeed } from '@/utils/rss'
 generateRssFeed()
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  if (process.env.NODE_ENV !== 'development') return (
-    <main className="flex justify-center items-center mx-auto max-w-4xl min-h-screen">
-      ⚠️ This website is under construction. Please, come back later.
-    </main>
-  )
-
   return (
     <>
       <main className={page()}>
