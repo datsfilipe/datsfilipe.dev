9f5ad81b9b0a4f1c170b6fb0f51222e7b9410e21
datsfilipe <datsfilipe.foss@proton.me>
2023-08-23 00:14:14 -0300

    chore(deps): add remark/rehype plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,37 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
-
 import tailwind from '@astrojs/tailwind'
+
+import remarkMath from 'remark-math'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), tailwind()]
+  integrations: [mdx({
+    extendMarkdownConfig: true
+  }), sitemap(), react(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: 'min-dark',
+      wrap: true
+    },
+    remarkPlugins: [
+      remarkMath,
+    ],
+    rehypePlugins: [
+      rehypeKatex,
+      rehypeSlug,
+      [rehypeAutolinkHeadings, {
+        behavior: 'wrap',
+        properties: {
+          className: ['anchor'],
+        },
+      }],
+    ],
+    gfm: true
+  },
 })
