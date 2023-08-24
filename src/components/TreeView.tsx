4a13781f05dc53c5d66efbed55d4216e9f36466a
datsfilipe <datsfilipe.foss@proton.me>
2023-08-24 10:55:30 -0300

    style(components): fix treeview items padding

--- src/components/TreeView.tsx
+++ src/components/TreeView.tsx
@@ -24,7 +24,7 @@ const Node = ({ children, isFolder, pathname, slug }: {
 
     return (
       <a
-        className={`flex flex-col pl-[2.3rem] py-1 border-l border-stone-700 hover:brightness-100 hover:border-blue-600 hover:text-blue-600 ${
+        className={`flex flex-col pl-[1.5rem] ml-[0.45rem] py-1 border-l border-stone-700 hover:brightness-100 hover:border-blue-600 hover:text-blue-600 ${
           pathname === href ? 'text-blue-600 !border-blue-600' : ''
         }`}
         href={href}
