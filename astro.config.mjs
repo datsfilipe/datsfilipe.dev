52500441ad1384638db1242a5b0e7481fa7409cb
datsfilipe <datsfilipe.foss@proton.me>
2023-08-25 20:15:20 -0300

    fix(*): bad installation of netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
-import netlify from '@astrojs/netlify/functions';
-
-import remarkMath from 'remark-math'
-import rehypeSlug from 'rehype-slug'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeKatex from 'rehype-katex'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import remarkMath from 'remark-math';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeKatex from 'rehype-katex';
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,19 +22,13 @@ export default defineConfig({
       theme: 'min-dark',
       wrap: true
     },
-    remarkPlugins: [
-      remarkMath,
-    ],
-    rehypePlugins: [
-      rehypeKatex,
-      rehypeSlug,
-      [rehypeAutolinkHeadings, {
-        behavior: 'wrap',
-        properties: {
-          className: ['anchor'],
-        },
-      }],
-    ],
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex, rehypeSlug, [rehypeAutolinkHeadings, {
+      behavior: 'wrap',
+      properties: {
+        className: ['anchor']
+      }
+    }]],
     gfm: true
   },
-})
+});
