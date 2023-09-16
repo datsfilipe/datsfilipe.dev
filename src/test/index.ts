0611c7c3e975817ccbd6d2770a70361f03f32b0c
datsfilipe <datsfilipe.foss@proton.me>
2023-09-16 02:59:15 -0300

    feat: add testing exports file

---
+++ src/test/index.ts
@@ -0,0 +1,4 @@
+import { beforeEach, describe, expect, test } from 'vitest'
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+
+export { beforeEach, describe, expect, test, render, screen, fireEvent, waitFor }
