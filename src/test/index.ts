0afd2bef8d8a8c5a7631236031fdb4914fa7caa4
datsfilipe <datsfilipe.foss@proton.me>
2023-09-17 14:13:30 -0300

    refactor: remove old folder with test file

--- src/test/index.ts
+++
@@ -1,4 +0,0 @@
-import { beforeEach, describe, expect, test } from 'vitest'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
-
-export { beforeEach, describe, expect, test, render, screen, fireEvent, waitFor }
