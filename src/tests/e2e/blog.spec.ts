15cd0eff8d588d5faf7912f2a91546ca38cf7901
datsfilipe <datsfilipe.foss@proton.me>
2023-09-18 08:28:59 -0300

    test: add tests for blog page

---
+++ src/tests/e2e/blog.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test'
+
+test('Page should exist', async ({ page }) => {
+  await page.goto('http://localhost:3000/blog')
+  expect(page).toBeDefined()
+})
+
+test('Page should have a title', async ({ page }) => {
+  await page.goto('http://localhost:3000/blog')
+  const title = await page.title()
+  expect(title).toBe('Filipe Lima')
+})
+
+test('Page should have valid links', async ({ page }) => {
+  await page.goto('http://localhost:3000/blog')
+
+  const links = await page.$$eval('main a', (as) =>
+    as.map((a) => a.getAttribute('href'))
+  )
+
+  for (const link of links) {
+    if (link === null) continue
+
+    expect(link).toBeDefined()
+    await page.goto(`http://localhost:3000${link}`)
+    expect(page.url()).toBe(`http://localhost:3000${link}`)
+  }
+})
