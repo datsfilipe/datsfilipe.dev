08316f8440661456619fd91727f49c8be24315e2
datsfilipe <datsfilipe.foss@proton.me>
2023-09-18 08:22:40 -0300

    test(e2e): add header links validation

--- src/tests/e2e/home.spec.ts
+++ src/tests/e2e/home.spec.ts
@@ -10,3 +10,21 @@ test('Page should have a title', async ({ page }) => {
   const title = await page.title()
   expect(title).toBe('Filipe Lima')
 })
+
+test('Header should have valid links', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  const links = await page.$$eval('header a', (as) =>
+    as.map((a) => a.getAttribute('href'))
+  )
+
+  for (const link of links) {
+    if (link === null) continue
+    if (link.startsWith('http')) continue
+    if (link.endsWith('.pdf')) continue
+
+    expect(link).toBeDefined()
+    await page.goto(`http://localhost:3000${link}`)
+    expect(page.url()).toBe(`http://localhost:3000${link}`)
+  }
+})
