6b25e41c4f8557c31734f35dce5485db83332e33
datsfilipe <datsfilipe.foss@proton.me>
2023-09-21 18:15:12 -0300

    fix: uncomment test and change port from 3000 to 4321
    That was that all the time.

--- src/tests/e2e/projects.spec.ts
+++ src/tests/e2e/projects.spec.ts
@@ -1,31 +1,31 @@
 import { test, expect } from '@playwright/test'
 
 test('Page should exist', async ({ page }) => {
-  await page.goto('http://localhost:3000/projects')
+  await page.goto('http://localhost:4321/projects')
   expect(page).toBeDefined()
 })
 
 test('Page should have a title', async ({ page }) => {
-  await page.goto('http://localhost:3000/projects')
+  await page.goto('http://localhost:4321/projects')
   const title = await page.title()
   expect(title).toBe('Filipe Lima')
 })
 
 test('Page should have valid links', async ({ page }) => {
-  await page.goto('http://localhost:3000/projects')
+  await page.goto('http://localhost:4321/projects')
 
   const links = await page.$$eval('main a', (as) =>
     as.map((a) => a.getAttribute('href'))
   )
 
-  const urlRegex = /^(http:\/\/localhost:3000|https?:\/\/[^\s/$.?#].[^\s]*)$/
+  const urlRegex = /^(http:\/\/localhost:4321|https?:\/\/[^\s/$.?#].[^\s]*)$/
 
   for (const link of links) {
     if (link !== null) {
       expect(
         link.startsWith('http')
           ? link
-          : `http://localhost:3000${link}`
+          : `http://localhost:4321${link}`
       ).toMatch(urlRegex)
     }
   }
