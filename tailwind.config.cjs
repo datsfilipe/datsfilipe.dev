b97d88a3821264752801df488a79c9dce10dc46c
datsfilipe <datsfilipe.foss@proton.me>
2024-01-03 13:29:19 -0300

    refactor: create a proper card for projects
    Basically, using images as they were was too complicated for many
    reasons:
    
    - Had to use tactics to avoid cors errors from websites
    - Duplicate info for each project
    - Too much images to load on a single page
    
    - Created a card similar to github og image for repositories

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      boxShadow: {
-        'card-inner': 'inset -12px -77px 116px -51px rgba(0,0,0,0.7)',
-      }
-    }
-  },
   plugins: [
     require('@tailwindcss/typography'),
   ]
