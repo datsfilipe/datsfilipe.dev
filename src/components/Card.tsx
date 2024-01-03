dcedec6b0fd6404b40034fb9838a2e6ecfbea8e6
datsfilipe <datsfilipe.foss@proton.me>
2024-01-03 13:48:40 -0300

    refactor(card): remove title and add a colorful bar in bottom

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -30,15 +30,10 @@ export default function Card ({ repo }: CardProps): ReactElement {
   const author = repo.url.split('/')[3]
 
   return (
-    <div className="flex flex-col space-y-4 pb-10">
-      <a href={repo.url} target="_blank" rel="noopener noreferrer">
-        <h3
-          className="text-md font-bold bg-stone-600 w-fit px-4 py-2"
-        >{repo.name}</h3>
-      </a>
+    <div className="flex flex-col space-y-4">
       <a href={repo.homepageUrl} target="_blank" rel="noopener noreferrer">
         <div
-          className="bg-[#fff] text-black flex justify-between min-h-[12rem] h-fit relative w-full overflow-hidden [&:hover>span]:mb-6 [&:hover>span>span]:flex rounded-lg"
+          className="bg-[#fff] text-black flex justify-between min-h-[12rem] h-fit relative w-full overflow-hidden [&:hover>span]:mb-6 [&:hover>span>span]:flex"
         >
           <div className="pt-6 pl-6 pb-6 pr-4 h-full">
             <h4 className="text-xl">{author}/<b>{repo.name}</b></h4>
@@ -53,6 +48,7 @@ export default function Card ({ repo }: CardProps): ReactElement {
           <span className="flex items-center space-x-2 absolute bottom-0 right-0 m-4 text-xl z-10 transition-all">
             <FaGithub className="w-6 h-6" /><span className="hidden text-sm font-medium transition-all">Visit Page</span>
           </span>
+          <div className="absolute bottom-0 left-0 w-full h-2 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"></div>
         </div>
       </a>
     </div>
